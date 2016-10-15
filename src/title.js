78c2c90858092ac49ec159cb8474115176ba0506
ishikura1 <ishikuratakuya00@gmail.com>
2016-10-15 18:12:13 +0900

    a

--- src/title.js
+++ src/title.js
@@ -18,7 +18,7 @@ var gamestart = cc.Layer.extend({
 		title_logo.setPosition(size.width/2,size.height * 0.3);
         	this.addChild(title_logo);
         	
-        	/*
+        	
         	// タップイベントリスナーを登録する
                 cc.eventManager.addListener({
                     event: cc.EventListener.TOUCH_ONE_BY_ONE,
@@ -26,34 +26,7 @@ var gamestart = cc.Layer.extend({
                     onTouchBegan: this.onTouchBegan,
                     onTouchMoved: this.onTouchMoved,
                     onTouchEnded: this.onTouchEnded
-                }, this);*/
-                
-                // タッチイベントに対応している
-if(window.TouchEvent){
-
-	// イベントリスナーに対応している
-	if(document.addEventListener){
-
-		// ------------------------------------------------------------
-		// タッチすると実行される関数
-		// ------------------------------------------------------------
-		function TouchEventFunc(e){
-			
-		}
-
-		// ------------------------------------------------------------
-		// リッスンを開始する
-		// ------------------------------------------------------------
-		// タッチを開始すると実行されるイベント
-		//document.addEventListener("touchstart",TouchEventFunc);
-
-		// タッチしたまま平行移動すると実行されるイベント
-		//document.addEventListener("touchmove",TouchEventFunc);
-
-		// タッチを終了すると実行されるイベント
-		document.addEventListener("touchend",onTouchEnded);
-	}
-}
+                }, this);
                 
 	},
 	onTouchBegan: function(touch, event) {
