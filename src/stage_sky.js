5ce961b976f2255466d81d5529c8871d5deed05a
ishikura1 <ishikuratakuya00@gmail.com>
2016-10-15 22:19:57 +0900

    a

--- src/stage_sky.js
+++ src/stage_sky.js
@@ -119,7 +119,7 @@ var game = cc.Layer.extend({
     //スポンサー様看板
     addSponserBoard: function(event) {
       var ground = new Ground();
-      ground.setScale(0.5);
+      ground.setScale(0.1);
       this.addChild(ground);
       var sponserboard = new SponserBoard();
       sponserboard.setScale(0.2);
@@ -358,18 +358,18 @@ var SponserLogo = cc.Sprite.extend({
     ctor:function() {
         this._super();
         this.initWithFile(res.sponser_logo_png);
-        //画像の描画開始位置
+        //背景画像の描画開始位置
+      this.setPosition(size.width/2,size.height * 0.25);
     },
     //onEnterメソッドはスプライト描画の際に必ず呼ばれる
     onEnter:function() {
-      this.setPosition(size.width/2,size.height * 0.25);
-      var moveAction = cc.MoveTo.create(3, new cc.Point(-200, this.getPosition().y));
-      this.runAction(moveAction);
       this.scheduleUpdate();
     },
     update:function(dt){
+      //座標を更新する
+        this.setPosition(this.getPosition().x-scrollSpeed,this.getPosition().y);
         //画面の外にでたアイテムを消去する処理
-      if (this.getPosition().x < -100) {
+      if (this.getPosition().x < -200) {
       gameLayer.removeObject(this)
       }
     },
