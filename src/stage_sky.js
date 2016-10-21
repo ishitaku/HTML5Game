62839d90ec95adee71d6d4f349f6cd3a9cce704d
ishikura <t.ishikura@plusplus.jp>
2016-10-21 16:54:51 +0900

    a

--- src/stage_sky.js
+++ src/stage_sky.js
@@ -9,8 +9,8 @@ var background_sky1;	//背景2
 var background_sky2;	//背景3
 var scrollSpeed_sky = 2.5;		//スクロール速度
 var player_sky;					//プレイヤー
-var gameGravity_sky = -0.06;	//重力
-var gameThrust_sky = 0.125;		//上昇力
+var gameGravity_sky = -0.05;	//重力
+var gameThrust_sky = 0.1;		//上昇力
 var life_sky;		//ライフ
 var score_sky = 0;		//スコア
 var life_Score_sky = 0;	//ライフが回復するスコア
